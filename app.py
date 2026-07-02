878356a2dc65b9a62f9888e415633d49b8e73fab
blueyonder-cy <yancey.cameron@gmail.com>
2026-07-02 13:22:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.subheader("📋 Flight Leg Details")
 
 col1, col2 = st.columns(2)
 with col1:
-   block_hours = st.number_input("Total Block Time (Hours)", min_value=0.0, max_value=30.0, value=23.8, step=0.1, help="Enter total block hours for the flight or sequence.")
+   block_hours = st.number_input("Total Block Time (Hours)", min_value=0.0, max_value=150.0, value=0, step=0.1, help="Enter total block hours for the flight or sequence.")
 with col2:
     # Default to HKT
     tz_choice = st.selectbox("Arrival Local Timezone", [
